28257dac30d58c16c8f82dbbf745f4fa3b839922
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-18 12:14:22 +0200

    af

---
+++ textsearch.php
@@ -0,0 +1,13 @@
+<?php
+
+$pattern = '/[a-zA-Z]{9,}/m';
+$input = file("script.txt");
+$implodedInput = implode($input);
+
+preg_match_all($pattern, $implodedInput, $matches, PREG_SET_ORDER, 0);
+
+foreach ($matches as $word) {
+    echo($word[0] . PHP_EOL);
+}
+
+//var_dump($matches);
